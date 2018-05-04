e22be34d6054e678cec2ed5f2a7e66fed6964517
Clint Modien <cmodien@gmail.com>
2018-05-04 10:35:26 -0700

    Adding support for multiple repos

--- README.md
+++ README.md
@@ -1,3 +1,70 @@
 # retarget-github-prs
 
-A quick example using github.com/google/go-github/github to retarget pull requests on github to a different branch.
+A quick example using golang and github.com/google/go-github/github to retarget pull requests on github to a different branch.
+
+## Setup
+
+You can install golang on Mac using Homebrew:
+
+Make sure XCode is installed:
+
+`xcode-select --install`
+
+Install Brew:
+
+`/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"`
+
+Install Go:
+
+`brew install go`
+
+Add golang env vars to your `~/.bash_profile` then source it.
+
+```bash
+echo 'export GOPATH="$HOME/code/go"' >> ~/.bash_profile
+echo 'export GOBIN="$GOPATH/bin"' >> ~/.bash_profile
+echo 'export PATH="$GOBIN:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
+```
+
+Clone this repo into your $GOPATH/src dir:
+
+```bash
+mkdir -p $GOPATH/src/github.com/clintmod
+cd $GOPATH/src/github.com/clintmod
+git clone git@github.com:clintmod/retarget-github-prs.git
+cd retarget-github-prs
+```
+
+Build the project:
+
+```bash
+  go get
+  go build
+```
+
+You should now have a binary in the dir `./retarget-github-prs`
+
+## Usage
+
+Make sure you GITHUB_USERNAME and GITHUB_PASSWORD have env vars set:
+
+```bash
+echo 'export GITHUB_USERNAME="YOUR_GITHUB_USERNAME"' >> ~/.bash_profile
+echo 'export GITHUB_PASSWORD="YOUR_GITHUB_PASSWORD"' >> ~/.bash_profile
+source ~/.bash_profile
+```
+
+Then you can call the executable like:
+
+`./retarget-github-prs <github account> <old branch> <new branch> <comma separated list of repos>`
+
+Example:
+
+`./retarget-github-prs clintmod master development repo1,repo2,repo3`
+
+This will retarget all prs in repo1, repo2 and repo3 that are targeted at master to the development branch.
+
+## Notes
+
+Github Account is seperate argument to specify a repository that you contribute to that's not your own.
--- main.go
+++ main.go
@@ -5,39 +5,79 @@ import (
 	"fmt"
 	"github.com/google/go-github/github"
 	"os"
+	"strings"
 )
 
-func main() {
-	tp := github.BasicAuthTransport{
-		Username: os.Getenv("GITHUB_USERNAME"),
-		Password: os.Getenv("GITHUB_PASSWORD"),
+func envVarError(name string) {
+	fmt.Errorf("No %v environment variable found", name)
+	os.Exit(1)
+}
+
+func missingArg(arg string, index int) {
+	fmt.Printf("Missing arg %v at position %d\n", arg, index)
+	os.Exit(1)
+}
+
+func validateArgs(args []string) {
+	if len(args) < 2 {
+		missingArg("Github Account", 1)
 	}
-	owner := "clintmod"
-	repo := "Oceans"
-	oldBranch := "master"
-	newBranch := "development"
+	if len(args) < 3 {
+		missingArg("Old Branch", 2)
+	}
+	if len(args) < 4 {
+		missingArg("New Branch", 3)
+	}
+	if len(args) < 5 {
+		missingArg("Repos (e.g. oceans,triton,rhode", 3)
+	}
+}
+
+func main() {
+	uname := os.Getenv("GITHUB_USERNAME")
+	pass := os.Getenv("GITHUB_PASSWORD")
+
+	if uname == "" {
+		envVarError("GITHUB_USERNAME")
+	}
+	if pass == "" {
+		envVarError("GITHUB_PASSWORD")
+	}
+
+	validateArgs(os.Args)
+
+	owner := os.Args[1]
+	oldBranch := os.Args[2]
+	newBranch := os.Args[3]
+	repos := strings.Split(os.Args[4], ",")
+
+	tp := github.BasicAuthTransport{Username: uname, Password: pass}
 
 	client := github.NewClient(tp.Client())
 
-	opt := &github.PullRequestListOptions{"open", "", oldBranch, "created", "desc", github.ListOptions{Page: 1}}
-	pulls, _, err := client.PullRequests.List(context.Background(), owner, repo, opt)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
-	}
-	numberOfPulls := len(pulls)
-	fmt.Println("number of pulls = ", numberOfPulls)
-	for i := 0; i < numberOfPulls; i++ {
-		pull := pulls[i]
-		pullNumber := *pull.Number
-		*pull.Base.Ref = newBranch
-		fmt.Printf("Retargeting pull request %v the %v branch\n", pullNumber, *pull.Base.Ref)
-		_, _, err := client.PullRequests.Edit(context.Background(), owner, repo, pullNumber, pull)
+	for _, repo := range repos {
+		opt := &github.PullRequestListOptions{"open", "", oldBranch, "created", "desc", github.ListOptions{Page: 1}}
+		pulls, _, err := client.PullRequests.List(context.Background(), owner, repo, opt)
+
 		if err != nil {
-			fmt.Errorf("%d: PullRequests.Edit returned error: %v", i, err)
-		} else {
-			fmt.Printf("pull request %v retargeted\n", pullNumber)
+			fmt.Printf("Error: %v\n", err)
+			return
 		}
 
+		numberOfPulls := len(pulls)
+		fmt.Println("number of pulls = ", numberOfPulls)
+		for i := 0; i < numberOfPulls; i++ {
+			pull := pulls[i]
+			pullNumber := *pull.Number
+			*pull.Base.Ref = newBranch
+			fmt.Printf("Retargeting pull request %v the %v branch\n", pullNumber, *pull.Base.Ref)
+			_, _, err := client.PullRequests.Edit(context.Background(), owner, repo, pullNumber, pull)
+			if err != nil {
+				fmt.Errorf("%d: PullRequests.Edit returned error: %v", i, err)
+			} else {
+				fmt.Printf("pull request %v retargeted\n", pullNumber)
+			}
+
+		}
 	}
 }
